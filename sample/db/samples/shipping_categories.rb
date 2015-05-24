d5adad3abb4ecdde0b23e6df8db05754739d2f37
lunrfarsde <lunrfarsde@gmail.com>
2015-05-25 00:22:14 +0300

    Rename 'Default' to 'Varsayılan'

--- sample/db/samples/shipping_categories.rb
+++ sample/db/samples/shipping_categories.rb
@@ -1,1 +1,6 @@
-Spree::ShippingCategory.find_or_create_by!(:name => "Varsayılan")
+sc = Spree::ShippingCategory.find_by(name: "Default")
+if sc
+  sc.update!(name: "Varsayılan")
+else
+  Spree::ShippingCategory.create!(:name => "Varsayılan")
+end
